aefcdabca597a451ae0b58e8afbe086c5bc38dce
monmokk <myeongjikang910814@gmail.com>
2022-06-28 15:17:53 +0900

    환경변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      envFilePath: (process.env.NODE_ENV === 'production')? '.production.env' : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
